7a4f7edc73757cea888c4f7312a46ae7ea7b4f6d
Allan Garcia <allan.garcia.agcs@gmail.com>
2021-04-07 12:03:38 -0300

    Deletando arquivos antigos

--- src/content.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(onButtonClicked);
-chrome.runtime.sendMessage({ isPageLoaded: 1 });
-
-async function onButtonClicked() {
-    const url = document.URL;
-
-    if (isExercise(url)) parseExercise(batchIdGenerator(), 1);
-    else if (isCompetition(url)) await parseCompetition();
-}
